c825be84e991a9a12869dffaf03f69fdd40aba1c
grouquette <ghislain.rouquette@outlook.fr>
2024-12-19 17:14:07 +0100

    modification des infos utilisateurs presque bon

--- src/main/java/fr/eni/projet_enchere/dal/UtilisateurDAOImpl.java
+++ src/main/java/fr/eni/projet_enchere/dal/UtilisateurDAOImpl.java
@@ -21,7 +21,7 @@ public class UtilisateurDAOImpl implements UtilisateurDAO {
 
 	private static final String FIND_BY_PSEUDO = "SELECT * FROM Utilisateurs WHERE pseudo = :pseudo";
 
-	private static final String UPDATE = "UPDATE UTILISATEURS SET pseudo = :pseudo, nom = :nom, prenom = :prenom, email = :email, telephone= :telephone, rue = :rue, code_postal = :codePostal, ville = :ville, mot_de_passe = :motDePasse WHERE no_utilisateur = :email";
+	private static final String UPDATE = "UPDATE UTILISATEURS SET pseudo = :pseudo, nom = :nom, prenom = :prenom, email = :email, telephone= :telephone, rue = :rue, code_postal = :codePostal, ville = :ville, mot_de_passe = :motDePasse WHERE pseudo = :pseudo";
 
 	private static final String FIND_ALL = "SELECT id, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur FROM Utilisateurs WHERE id = :id";
 	
@@ -98,7 +98,7 @@ public class UtilisateurDAOImpl implements UtilisateurDAO {
 		map.addValue("rue", utilisateur.getRue());
 		map.addValue("codePostal", utilisateur.getCodePostal());
 		map.addValue("ville", utilisateur.getVille());
-		map.addValue("mdp", utilisateur.getMotDePasse());
+		map.addValue("motDePasse", utilisateur.getMotDePasse());
 
 		this.jdbcTemplate.update(UPDATE, map);
 	}
